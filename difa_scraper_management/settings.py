7c86af0c7851cd0c46510968b767ecb7b4fc389e
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-05-31 11:38:18 -0700

    updated settings to allow all hosts

--- difa_scraper_management/settings.py
+++ difa_scraper_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5_1(sw_!di_yybrc_w2p#zrg#ep&m3gv6srem8y%9d*si%q1vm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
